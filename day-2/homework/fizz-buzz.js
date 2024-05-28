335ac7cbce1bc55c907b8e57cadd0f4ad937606f
jhernandez504 <jeremy.hernandez504@gmail.com>
2024-05-28 11:45:54 -0500

    fixed fizzbuzz()

--- day-2/homework/fizz-buzz.js
+++ day-2/homework/fizz-buzz.js
@@ -13,12 +13,12 @@
 function fizzBuzz() {
     // YOUR CODE GOES BELOW HERE //
     //for loop to iterate thru 1-100 and print
-    for (let i = 0; i <= 100; i++) {
+    for (let i = 1; i <= 100; i++) {
         if (i % 5 === 0 && i % 3 === 0){
             console.log('FizzBuzz');
-        } if (i % 3 === 0){
+        } else if (i % 3 === 0){
             console.log('Fizz');
-        }   if ( i % 5 === 0){
+        }   else if ( i % 5 === 0){
                 console.log('Buzz');
         }   else{
                 console.log(i);
